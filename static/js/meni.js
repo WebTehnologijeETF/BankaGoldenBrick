2f71390e7b7d1b319c6a883fd5b7e00637f8dcd1
mmujic <mmujic1@etf.unsa.ba>
2015-04-08 18:47:11 +0200

    Update validacije i stila kontakt forme
    Poboljšana validacija, dodani eventi za text change i mouseout, promjena
    stila inputa na kontatk formi.

--- static/js/meni.js
+++ static/js/meni.js
@@ -15,7 +15,20 @@ window.onload = function(){
 	var page = path.split("/").pop();
 	if(page=="contact.html"){
 		document.kontaktForma.telefon.setAttribute("placeholder", "+??? ?? ???-???");
+		/*var elementi = document.getElementsByTagName("input");
+		for(var i=0; i < elementi.length; i++){
+			if(elementi[i].getAttribute("type")!="range" && elementi[i].getAttribute("type")!="submit"){
+				
+		console.log(elementi);*/
 	}
+	var vrh = document.getElementsByClassName("vrh");
+	vrh = vrh[0];
+	vrh = vrh.childNodes[1];
+	vrh.addEventListener("click", function(ev){
+		ev.preventDefault();
+		idiNaVrh(); 
+		return false;
+	});
 }
 
 function prikaziPadajuci(obj){
@@ -24,4 +37,12 @@ function prikaziPadajuci(obj){
 }
 function sakrijPadajuci(obj){
 	obj.children[1].style.visibility="hidden";
+}
+var timeOut;
+function idiNaVrh(){
+  	if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
+    window.scrollBy(0,-50);
+    timeOut=setTimeout('idiNaVrh()',20);
+  	}
+  	else clearTimeout(timeOut);
 }
--- static/js/validacija.js
+++ static/js/validacija.js
@@ -2,89 +2,75 @@ function validacija(){
     //Provjera da li su obavezna polja popunjena
 	var elements = document.getElementsByTagName("input");
 	for (var i = 0; i < elements.length; i++){
-        if(elements[i].classList.contains("obavezan")){
-            if(elements[i].value == null || elements[i].value == ""){
-                prikaziGresku(elements[i], "Polje je obavezno!");
-                return false;
-            }
-        }
-    }
-	//Validacija imena (samo slova)
-	var ulaz = document.kontaktForma.ime;
-	var reg = /[a-zščćđž]{3,}$/i;
-	if(!reg.test(ulaz.value)){
-		prikaziGresku(ulaz, "Pogrešno ime!");
-		return false;
-	}
-	//Validacija prezimena (samo slova)
-	ulaz = document.kontaktForma.prezime;
-	reg = /[a-zćčđšž]{3,}$/i;
-	if(!reg.test(ulaz.value)){
-		prikaziGresku(ulaz, "Pogrešno prezime!");
-		return false;
-	}
-	//Validacija adrese (slova i brojevi sa razmacima)
-	ulaz = document.kontaktForma.adresa;
-	if(ulaz.value != null && ulaz.value != ""){
-		reg = /[a-zćčđšž0-9\s,.]{4,}$/i;
-		if(!reg.test(ulaz.value)){
-			prikaziGresku(ulaz, "Pogrešna adresa!");
-			return false;
+		if(elements[i].getAttribute("type") != "submit" && elements[i].getAttribute("type") != "range"){
+			elements[i].style.outline="none";
+			elements[i].addEventListener("input", function(){
+			if(validiraj(this, false))
+				this.style.boxShadow="0 0 1pt 1pt #69CF8E";
+			else{
+				this.style.boxShadow="0 0 1pt 1pt #FF294D";
+				this.classList.add("nevaljao");
+			}
+			});
+			elements[i].addEventListener("focusout", function(){
+				if(validiraj(this, false)){
+					this.style.boxShadow="none";
+					this.style.border="1px solid #2d97af";
+				}
+			});
+			elements[i].addEventListener("blur", function(){
+				if(validiraj(this, false)){
+					this.style.boxShadow="none";
+					this.style.border="1px solid #2d97af";
+				}
+			});
+			elements[i].addEventListener("focus", function(){
+				if(!this.classList.contains("nevaljao")){
+					this.style.boxShadow="0 0 1pt 1pt dodgerblue";
+					this.style.border="none";
+				   }
+			});
+			var validno = validiraj(elements[i], true);
+			if(!validno)
+				return false;
 		}
-	}
-	//Validacija email-a regexom
-	//Najjednostavnije mora imati barem jedan znak prije @
-	//najmanje jedan poslije, tačku . i najmanje dva znaka
-	//u domeni
-	ulaz = document.kontaktForma.email;
-	reg = /\S+@\S+\.\S{2,}/;
-	if(!reg.test(ulaz.value)){
-		prikaziGresku(ulaz, "Email je pogrešan!");
-		return false;
-	}
-	//Validacija potvrdnog email-a regexom
-	//Najjednostavnije mora imati barem jedan znak prije @
-	//najmanje jedan poslije, tačku . i najmanje dva znaka
-	//u domeni
-	ulaz = document.kontaktForma.emailPotvrda;
-	reg = /\S+@\S+\.\S{2,}/;
-	if(!reg.test(ulaz.value)){
-		prikaziGresku(ulaz, "Email je pogrešan!");
-		return false;
-	}
-	//Cros validacija potvrdnog emaila
-	//Poredi se sa prvim emailom
-	var potvrda = document.getElementById("emailPotvrdaID");
-	var email = document.kontaktForma.email;
-	if(potvrda.value != email.value){
-		prikaziGresku(potvrda, "Email nije isti!");
-		return false;
-	}
-	//Validacija broja telefona pomoću regexa
-	//Broj mora biti u standardnom obliku +*** ** ***-*** (*-broj)
-	ulaz = document.kontaktForma.telefon;
-	if(ulaz.value != null && ulaz.value != ""){
-		reg = /\+\d{3}\s\d\d\s\d{3}\-\d{3}$/;
-		if(!reg.test(ulaz.value)){
-			prikaziGresku(ulaz, "Pogrešan format!(+??? ?? ???-???)");
-			return false;
-		}
-	}
-	//Provjera da li je polje poruka obavezno
-	//i u slučaju da jeste provjera da li je prazno
-	ulaz = document.kontaktForma.poruka;
-    if(ulaz.classList.contains("obavezan")){
-        if(ulaz.value == null || ulaz.value == ""){
-            prikaziGresku(ulaz, "Polje je obavezno!");
-            return false;
-        }
     }
-    
-    return true;
+	var message = document.kontaktForma.poruka;
+	message.style.outline="none";
+	message.addEventListener("input", function(){
+	if(validiraj(this, false))
+		this.style.boxShadow="0 0 1pt 1.5pt #69CF8E";
+	else{
+		this.style.boxShadow="0 0 1pt 1pt #FF294D";
+		this.classList.add("nevaljao");
+	}
+	});
+	message.addEventListener("focusout", function(){
+		if(validiraj(this, false)){
+			this.style.boxShadow="none";
+			this.style.border="1px solid #2d97af";
+		}
+	});
+	message.addEventListener("blur", function(){
+		if(validiraj(this, false)){
+			this.style.boxShadow="none";
+			this.style.border="1px solid #2d97af";
+		}
+	});
+	message.addEventListener("focus", function(){
+		if(!this.classList.contains("nevaljao")){
+			this.style.boxShadow="0 0 1pt 1pt dodgerblue";
+			this.style.border="none";
+		}
+	});
+	return validiraj(message, true);
 }
 
 
 function prikaziGresku(element, string){
+	element.style.boxShadow="0 0 1pt 1pt #FF294D";
+	element.style.border="none";
+	element.classList.add("nevaljao");
 	element.focus();
 	var ime = element.getAttribute("name") + "_error";
 	var slika = document.getElementById(ime);
@@ -92,8 +78,111 @@ function prikaziGresku(element, string){
 	var temp = element.getAttribute("name") + "_greska";				
 	var labela = document.getElementById(temp);
 	labela.innerHTML = string;
-	element.addEventListener("input", function(){
+}
+
+function validiraj(el, ispis){
+	if(!el.classList.contains("obavezan") && (el.value==null || el.value=="")){
+		var ime = el.getAttribute("name") + "_error";
+		var slika = document.getElementById(ime);
 		slika.style.display = "none";
+		var temp = el.getAttribute("name") + "_greska";				
+		var labela = document.getElementById(temp);
 		labela.innerHTML = "";
-	});
+	   return true;
+	}
+	else if(el.classList.contains("obavezan") && (el.value==null || el.value=="") && ispis){
+		prikaziGresku(el, "Polje je obavezno!");
+		return false;
+	}
+	else if(el.classList.contains("obavezan") && (el.value==null || el.value==""))
+		return false;
+	else{
+		if(el.getAttribute("name")=="ime"){
+			//Validacija imena (samo slova)
+			var ulaz = document.kontaktForma.ime;
+			var reg = /[a-zščćđž]{3,}$/i;
+			if(!reg.test(ulaz.value)){
+				if(ispis)
+					prikaziGresku(ulaz, "Pogrešno ime!");
+				return false;
+			}
+		}
+		if(el.getAttribute("name")=="prezime"){
+			//Validacija prezimena (samo slova)
+			ulaz = document.kontaktForma.prezime;
+			reg = /[a-zćčđšž]{3,}$/i;
+			if(!reg.test(ulaz.value)){
+				if(ispis)
+					prikaziGresku(ulaz, "Pogrešno prezime!");
+				return false;
+			}
+		}
+		if(el.getAttribute("name")=="adresa"){
+			//Validacija adrese (slova i brojevi sa razmacima)
+			ulaz = document.kontaktForma.adresa;
+			if(ulaz.value != null && ulaz.value != ""){
+				reg = /[a-zćčđšž0-9\s,.]{4,}$/i;
+				if(!reg.test(ulaz.value)){
+					if(ispis)
+						prikaziGresku(ulaz, "Pogrešna adresa!");
+					return false;
+				}
+			}
+		}
+		if(el.getAttribute("name")=="email"){
+			//Validacija email-a regexom
+			//Najjednostavnije mora imati barem jedan znak prije @
+			//najmanje jedan poslije, tačku . i najmanje dva znaka
+			//u domeni
+			ulaz = document.kontaktForma.email;
+			reg = /\S+@\S+\.\S{2,}/;
+			if(!reg.test(ulaz.value)){
+				if(ispis)
+					prikaziGresku(ulaz, "Email je pogrešan!");
+				return false;
+			}
+		}
+		if(el.getAttribute("name")=="emailPotvrda"){
+			//Validacija potvrdnog email-a regexom
+			//Najjednostavnije mora imati barem jedan znak prije @
+			//najmanje jedan poslije, tačku . i najmanje dva znaka
+			//u domeni
+			ulaz = document.kontaktForma.emailPotvrda;
+			reg = /\S+@\S+\.\S{2,}/;
+			if(!reg.test(ulaz.value)){
+				if(ispis)
+					prikaziGresku(ulaz, "Email je pogrešan!");
+				return false;
+			}
+			//Cros validacija potvrdnog emaila
+			//Poredi se sa prvim emailom
+			var potvrda = document.getElementById("emailPotvrdaID");
+			var email = document.kontaktForma.email;
+			if(potvrda.value != email.value){
+				if(ispis)
+					prikaziGresku(potvrda, "Email nije isti!");
+				return false;
+			}
+		}
+		if(el.getAttribute("name")=="telefon"){
+			//Validacija broja telefona pomoću regexa
+			//Broj mora biti u standardnom obliku +*** ** ***-*** (*-broj)
+			ulaz = document.kontaktForma.telefon;
+			reg = /^\+\d{3}\s\d\d\s\d{3}\-\d{3}$/;
+			if(!reg.test(ulaz.value)){
+				if(ispis)
+					prikaziGresku(ulaz, "Pogrešan format!(+??? ?? ???-???)");
+				return false;
+			}
+		}
+	}
+	//console.log(el);
+	el.classList.remove("nevaljao");
+	var ime = el.getAttribute("name") + "_error";
+	var slika = document.getElementById(ime);
+	slika.style.display = "none";
+	var temp = el.getAttribute("name") + "_greska";				
+	var labela = document.getElementById(temp);
+	labela.innerHTML = "";
+	return true;
 }
